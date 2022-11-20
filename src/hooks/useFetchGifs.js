75adfac23553b119b7ffefd1823a0b07d4563ce1
algonzalez <gonzalezhenry95@gmail.com>
2022-11-20 06:44:30 -0400

    arreglo de imagenes en produccion y cambio en callback

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import {  useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifs';
 
 export const useFetchGifs = (category) => {
@@ -6,16 +6,24 @@ export const useFetchGifs = (category) => {
     const [images, setImages] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
 
-    const getImages = useCallback(async () => {
-            const newImages = await getGifs(category);
-            setImages(newImages);
-            setIsLoading(false);
-        },[category])
+    // const getImages = useCallback(async () => {
+    //         const newImages = await getGifs(category);
+    //         setImages(newImages);
+    //         setIsLoading(false);
+    //     },[category])
     
         /** para no renderizar las consultas de bd cada vez que hay cambio en el objeto */
     useEffect(() => { // NO puedo devoolver una promesa, pero si una funcion
+
+
+        const getImages = async () => {
+            const newImages = await getGifs(category);
+            setImages(newImages);
+            setIsLoading(false);
+        }
+
         getImages();
-    }, [getImages])
+    }, [category])
     
 
   return { // devuelve un objeto no un hmtl como jsx
